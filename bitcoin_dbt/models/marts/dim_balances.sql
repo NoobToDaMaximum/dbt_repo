c7900f7fd0e9ff88fdbf17d4df837e7b728a8b41
Eduardo <eduardo.sebastian.hunerberg@gmail.com>
2025-09-18 09:50:21 +0200

    Final Fixes

--- bitcoin_dbt/models/marts/dim_balances.sql
+++ bitcoin_dbt/models/marts/dim_balances.sql
@@ -5,27 +5,43 @@
 ) }}
 
 WITH
-  -- Extract and combine all addresses and their values from both inputs and outputs
+  -- CTE to extract and combine all addresses and their values from both inputs and outputs
   all_addresses_and_values AS (
     -- Extract values from outputs
     SELECT
-      outputs.addresses AS `address`,
+      unnested_addresses AS `address`,
       outputs.value AS `value`,
-      t.transaction_id AS transaction_id,
-      t.is_coinbase
+      t.transaction_id AS transaction_id
     FROM {{ ref('stg_transactions') }} AS t,
-    unnest(t.outputs) AS outputs
+    UNNEST(t.outputs) AS outputs,
+    UNNEST(outputs.addresses) AS unnested_addresses
 
     UNION ALL
 
     -- Extract values from inputs (treated as negative values for balance calculation)
     SELECT
-      inputs.addresses AS `address`,
+      unnested_addresses AS `address`,
       -inputs.value AS `value`,
-      t.transaction_id AS transaction_id,
-      t.is_coinbase
+      t.transaction_id AS transaction_id
     FROM {{ ref('stg_transactions') }} AS t,
-    unnest(t.inputs) AS inputs
+    UNNEST(t.inputs) AS inputs,
+    UNNEST(inputs.addresses) AS unnested_addresses
+  ),
+
+  -- CTE to get a list of all transactions involved in a coinbase transaction
+  coinbase_transactions AS (
+    SELECT
+      `hash` AS transaction_id
+    FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`
+    WHERE is_coinbase IS TRUE
+  ),
+
+  -- CTE to get a list of all addresses involved in a coinbase transaction
+  coinbase_addresses AS (
+    SELECT DISTINCT
+      address
+    FROM all_addresses_and_values
+    WHERE transaction_id IN (SELECT transaction_id FROM coinbase_transactions)
   ),
 
   -- Final balance calculation, excluding coinbase-related addresses
@@ -34,7 +50,7 @@ WITH
       `address`,
       SUM(`value`) AS current_balance
     FROM all_addresses_and_values
-    WHERE NOT is_coinbase
+    WHERE `address` NOT IN (SELECT `address` FROM coinbase_addresses)
     GROUP BY `address`
   )
 
